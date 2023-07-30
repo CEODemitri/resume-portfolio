0353fe3466d74664fc3227f29b6621e731d07bd4
Color Codex <colorcodex8@Colors-iMac.local>
2023-07-29 21:02:22 -0500

    slow update

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -40,11 +40,11 @@ export default async function Home() {
           <article className="w-full h-1/3 bg-blue-200">Average</article>
           <article className="w-full h-1/3 bg-purple-400">Proficient</article>
         </div>
-        <a className="h-24 w-36 text-white bg-slate-800 m-auto">Resume</a>
+        <a className="h-10 w-36 text-white text-xl font-semibold px-6 py-1 justify-center bg-gradient-to-l from-slate-800 to-slate-300 hover:from-purple-500 hover:to-blue-400 flex m-auto mt-14 rounded-md hover:animate-bounce">Resume</a>
      </section>
    
   {/* Game */}
-    <div className="container max-w:1/3 h-72 mt-2 place-content-center flex">
+    <div className="container max-w:1/3 h-72 mt-16 place-content-center grid grid-cols-2">
       <section className="w-1/2 h-1/2 bg-purple-400">Banana</section>
       <section className="w-1/2 h-1/2 bg-blue-200">Orange</section>
       <section className="w-1/2 h-1/2 bg-green-400">Green</section>
