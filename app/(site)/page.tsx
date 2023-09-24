c7d254628609ff4fe623e8bb9568f6244c5eb19a
Color Codex <colorcodex8@Colors-iMac.local>
2023-09-24 17:13:55 -0500

    minor updates

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -77,24 +77,26 @@ export default async function Home() {
       {/* Skills Section */}
       <section className="">
         <h2 className="font-bold text-3xl text-gray-700 pb-5">Skills</h2>
-          <div className="grid grid-cols-3">
-            <section className="w-full h-1/3 rounded-md m-5 inline">
+          <div className="grid grid-rows-3">
+            <section className="w-full h-1/3 rounded-md m-5 gap-7">
               {skills.map((skill) => (
                 <>
-                  {skill.image && (
-                    <Image
-                      src={skill.image}
-                      alt={skill.name}
-                      width={50}
-                      height={50}
-                      className=""
-                    />
-                  )}
-                  <div className="mt-2 font-extrabold bg-gradient-to-r from-green-500 via-blue-300 to-purple-500 bg-clip-text text-transparent text-xl">
-                    {skill.name}
-                  </div>
-                  <div className="mt-2 font-extrabold text-green-700">
-                    {skill.rating}
+                  <div className="border inline-block">
+                    {skill.image && (
+                      <Image
+                        src={skill.image}
+                        alt={skill.name}
+                        width={50}
+                        height={50}
+                        className=""
+                      />
+                    )}
+                    <div className="mt-2 font-extrabold bg-gradient-to-r from-green-500 via-blue-300 to-purple-500 bg-clip-text text-transparent text-xl">
+                      {skill.name}
+                    </div>
+                    <div className="mt-2 font-extrabold text-green-700">
+                      {skill.rating}
+                    </div>
                   </div>
                 </>
               ))}
