b5a096cf545ed0ec3358eaa00bd68f62dd2304cd
CEODemitri <126187234+CEODemitri@users.noreply.github.com>
2024-01-06 16:04:59 -0600

    Update next.config.js
    🤞🏾 100× a charm

--- next.config.js
+++ next.config.js
@@ -4,6 +4,20 @@ const isProd = process.env.NODE_ENV === 'production'
 module.exports = {
   // Use the CDN in production and localhost for development.
   assetPrefix: isProd ? 'https://cdn.mydomain.com' : '',
-  // Remove the useCdn option as it is deprecated and can cause issues with next/image
-  // useCdn: false,
+  // Remove the experimental option as it is unstable and may cause issues with next/image
+  // experimental: {
+  //   appDir: true,
+  // },
+  images: {
+    // Remove the remotePatterns property as it restricts the sources of images that can be optimized by Next.js
+    // remotePatterns: [
+    //   {
+    //     protocol: 'https',
+    //     hostname: 'cdn.sanity.io',
+    //     port: '',
+    //   },
+    // ],
+    // Use the domains property instead to specify which domains are allowed to serve images for your Next.js application
+    domains: ['cdn.sanity.io'],
+  },
 }
