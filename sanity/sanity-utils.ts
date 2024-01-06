251321d91cc83a781f7c720f356e28791c00ff99
ceoDemitri <demichole@gmail.com>
2024-01-06 17:05:45 -0600

    this should be the fix

--- sanity/sanity-utils.ts
+++ sanity/sanity-utils.ts
@@ -1,7 +1,7 @@
 import { Project } from "@/types/Project";
 import { createClient, groq } from "next-sanity";
 import clientConfig from "./config/client-config";
-import { Page } from "@/types/page";
+import { Page } from "@/types/Page";
 import { Contact } from "@/types/Contact";
 import { Skill } from "@/types/Skill";
 
