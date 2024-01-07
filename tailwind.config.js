d3453d8aa6164592f7649307ca4b36e7f33451f1
ceoDemitri <demichole@gmail.com>
2024-01-07 09:00:23 -0600

    troubleshooting, commenting pieces of code at a time

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,22 @@
 /** @type {import('tailwindcss').Config} */
-export const content = [
-  "./app/**/*.{js,ts,jsx,tsx}",
-  "./pages/**/*.{js,ts,jsx,tsx}",
-  "./components/**/*.{js,ts,jsx,tsx}",
+const withMT = require("@material-tailwind/react/utils/withMT");
 
-  // Or if using `src` directory:
-  "./src/**/*.{js,ts,jsx,tsx}",
-];
-export const theme = {
-  extend: {},
-};
-export const plugins = [];
+module.exports = withMT({
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./node_modules/@material-tailwind/react/components/**/*.{js,ts,jsx,tsx}",
+    "./node_modules/@material-tailwind/react/theme/components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      backgroundImage: {
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+    },
+  },
+  plugins: [],
+});
