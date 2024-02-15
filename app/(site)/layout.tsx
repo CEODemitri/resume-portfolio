1557c2439159714c5e5b597201c5041086dd7121
ceoDemitri <demichole@gmail.com>
2024-02-15 08:03:46 -0600

    add icon to add menu tab, not functional

--- app/(site)/layout.tsx
+++ app/(site)/layout.tsx
@@ -40,6 +40,9 @@ export default async function RootLayout({
                 </Link>
               ))}
             </div>
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6 md:hidden">
+                <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5M12 17.25h8.25" />
+            </svg>
           </header>
           <main>{children}</main>
         </body>
